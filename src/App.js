4ff47369ed8720a6dd4362605c0e08b560707782
MustafaAliBangash <MustafaAliBangash@github.com>
2021-01-06 12:17:26 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from 'react';
+
+function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect( () => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/mustafaalibangash/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+    // "https://api.github.com/users/muhammadmohsin/repos"
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     console.log(json);
+    //     setData(json);
+    //   })
+  },[])
+   
+  return (
+    <div className="App">
+      <h1>You are seeing all repositories</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
